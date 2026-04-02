60790631cf044f50311d04aa251f5bd7805a5357
Doyun9380 <110959532+Doyun9380@users.noreply.github.com>
2026-04-02 16:10:15 +0900

    Update PokemonFollowing.rb

--- Scripts/Reborn/PokemonFollowing.rb
+++ Scripts/Reborn/PokemonFollowing.rb
@@ -110,4 +110,100 @@ Events.onMapUpdate += proc { |sender, e|
     $PokemonTemp.followingStarted = true
   end
 }
+  ################################################################################
+# Pokemon Following Feature
+################################################################################
+
+class PokemonTemp
+  attr_accessor :followingStarted
+end
+
+module PokemonFollowing
+  FOLLOWING_ENABLED = true
+  SPRITE_PATH = "Graphics/Characters/"
+
+  def self.getFollowSprite(pokemon)
+    return nil if !pokemon
+    species = pokemon.species
+    filename = sprintf("Follow%03d", species)
+    if pbResolveBitmap(SPRITE_PATH + filename)
+      return filename
+    end
+    return nil
+  end
+
+  def self.startFollowing
+    return if !FOLLOWING_ENABLED
+    return if !$Trainer || $Trainer.party.length == 0
+    stopFollowing
+    pokemon = $Trainer.party[0]
+    return if !pokemon || pokemon.isEgg?
+    sprite = getFollowSprite(pokemon)
+    return if !sprite
+    pbCreateFollowingPokemon(sprite)
+  end
+
+  def self.stopFollowing
+    pbRemoveDependency2("FollowingPokemon")
+  end
+
+  def self.refresh
+    return if !FOLLOWING_ENABLED
+    startFollowing
+  end
+end
+
+def pbCreateFollowingPokemon(spriteName)
+  return if !$game_map || !$game_player
+  return if !$MapFactory
+  x = $game_player.x
+  y = $game_player.y
+  case $game_player.direction
+  when 2 then y -= 1
+  when 4 then x += 1
+  when 6 then x -= 1
+  when 8 then y += 1
+  end
+  x = [[x, 0].max, $game_map.width-1].min
+  y = [[y, 0].max, $game_map.height-1].min
+  rpgEvent = RPG::Event.new(x, y)
+  rpgEvent.id = -9999
+  rpgEvent.name = "FollowingPokemon"
+  rpgEvent.pages[0].graphic.character_name = spriteName
+  rpgEvent.pages[0].move_type = 0
+  rpgEvent.pages[0].through = false
+  newEvent = Game_Event.new($game_map.map_id, rpgEvent, $game_map)
+  newEvent.character_name = spriteName
+  newEvent.moveto(x, y)
+  newEvent.through = false
+  $game_map.events[-9999] = newEvent
+  eventData = [
+    $game_map.map_id, -9999, $game_map.map_id,
+    x, y, $game_player.direction,
+    spriteName, 0, "FollowingPokemon", nil
+  ]
+  newRealEvent = $PokemonTemp.dependentEvents.createEvent(eventData)
+  $PokemonGlobal.dependentEvents.push(eventData)
+  $PokemonTemp.dependentEvents.instance_variable_get(:@realEvents).push(newRealEvent)
+  $PokemonTemp.dependentEvents.instance_variable_set(:@lastUpdate,
+    $PokemonTemp.dependentEvents.lastUpdate + 1)
+end
+
+Events.onMapSceneChange += proc { |sender, e|
+  mapChanged = e[1]
+  if mapChanged
+    PokemonFollowing.refresh
+  end
+}
+
+Events.onEndBattle += proc { |sender, e|
+  PokemonFollowing.refresh
+}
+
+Events.onMapUpdate += proc { |sender, e|
+  if !$PokemonTemp.followingStarted
+    PokemonFollowing.startFollowing
+    $PokemonTemp.followingStarted = true
+  end
+}
   
