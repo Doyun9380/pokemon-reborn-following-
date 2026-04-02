50024b94ea1d1ba25a299ce044a563fcad3860c6
Doyun9380 <110959532+Doyun9380@users.noreply.github.com>
2026-04-02 16:01:08 +0900

    Update PokemonFollowing.rb

--- Scripts/PokemonFollowing.rb
+++ Scripts/PokemonFollowing.rb
@@ -1,6 +1,5 @@
 ################################################################################
 # Pokemon Following Feature
-# 대표 포켓몬이 플레이어 뒤를 따라다니는 기능
 ################################################################################
 
 class PokemonTemp
@@ -15,25 +14,28 @@ module PokemonFollowing
     return nil if !pokemon
     species = pokemon.species
     filename = sprintf("Follow%03d", species)
+    echoln "Following: 스프라이트 찾는중 - #{filename}"
     if pbResolveBitmap(SPRITE_PATH + filename)
+      echoln "Following: 스프라이트 찾음! - #{filename}"
       return filename
     end
-    filename2 = "Follow" + PBSpecies.getName(species).gsub(" ","")
-    if pbResolveBitmap(SPRITE_PATH + filename2)
-      return filename2
-    end
+    echoln "Following: 스프라이트 없음 - #{filename}"
     return nil
   end
 
   def self.startFollowing
+    echoln "Following: startFollowing 호출됨"
     return if !FOLLOWING_ENABLED
     return if !$Trainer || $Trainer.party.length == 0
     stopFollowing
     pokemon = $Trainer.party[0]
+    echoln "Following: 포켓몬 = #{pokemon ? pokemon.name : 'nil'}"
     return if !pokemon || pokemon.isEgg?
     sprite = getFollowSprite(pokemon)
+    echoln "Following: 스프라이트 결과 = #{sprite}"
     return if !sprite
     pbCreateFollowingPokemon(sprite)
+    echoln "Following: 생성 완료!"
   end
 
   def self.stopFollowing
@@ -46,14 +48,11 @@ module PokemonFollowing
   end
 end
 
-################################################################################
-# 코드로 직접 이벤트 생성
-################################################################################
 def pbCreateFollowingPokemon(spriteName)
+  echoln "Following: pbCreateFollowingPokemon 호출됨 - #{spriteName}"
   return if !$game_map || !$game_player
   return if !$MapFactory
 
-  # 플레이어 뒤 위치
   x = $game_player.x
   y = $game_player.y
   case $game_player.direction
@@ -65,7 +64,6 @@ def pbCreateFollowingPokemon(spriteName)
   x = [[$game_player.x, x].max, 0].max
   y = [[$game_player.y, y].max, 0].max
 
-  # RPG::Event 직접 생성
   rpgEvent = RPG::Event.new(x, y)
   rpgEvent.id = -9999
   rpgEvent.name = "FollowingPokemon"
@@ -73,14 +71,43 @@ def pbCreateFollowingPokemon(spriteName)
   rpgEvent.pages[0].move_type = 0
   rpgEvent.pages[0].through = false
 
-  # Game_Event 생성
   newEvent = Game_Event.new($game_map.map_id, rpgEvent, $game_map)
   newEvent.character_name = spriteName
   newEvent.moveto(x, y)
   newEvent.through = false
 
-  # $game_map에 등록
   $game_map.events[-9999] = newEvent
 
-  # DependentEvents에 등록
-  e
+  eventData = [
+    $game_map.map_id, -9999, $game_map.map_id,
+    x, y, $game_player.direction,
+    spriteName, 0, "FollowingPokemon", nil
+  ]
+  newRealEvent = $PokemonTemp.dependentEvents.createEvent(eventData)
+  $PokemonGlobal.dependentEvents.push(eventData)
+  $PokemonTemp.dependentEvents.instance_variable_get(:@realEvents).push(newRealEvent)
+  $PokemonTemp.dependentEvents.instance_variable_set(:@lastUpdate,
+    $PokemonTemp.dependentEvents.lastUpdate + 1)
+  echoln "Following: 이벤트 등록 완료!"
+end
+
+Events.onMapSceneChange += proc { |sender, e|
+  mapChanged = e[1]
+  echoln "Following: onMapSceneChange - mapChanged=#{mapChanged}"
+  if mapChanged
+    PokemonFollowing.refresh
+  end
+}
+
+Events.onEndBattle += proc { |sender, e|
+  PokemonFollowing.refresh
+}
+
+Events.onMapUpdate += proc { |sender, e|
+  if !$PokemonTemp.followingStarted
+    echoln "Following: 첫 실행 시도"
+    PokemonFollowing.startFollowing
+    $PokemonTemp.followingStarted = true
+  end
+}
+  
